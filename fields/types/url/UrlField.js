32e9cb55d9721ed5f539a2c283027b07d596dc20
William Chiu <williamchiu16@gmail.com>
2016-08-02 16:17:17 -0400

    add disabled link

--- fields/types/url/UrlField.js
+++ fields/types/url/UrlField.js
@@ -8,6 +8,10 @@ module.exports = Field.create({
 		var styles = {
 			height: this.props.height
 		};
+
+		if (this.props.disabled) return <div><input disabled name={this.props.path} styles={styles} ref="focusTarget" value={this.props.value} className="form-control" /><a href={this.props.value} className="btn btn-link btn-goto-linked-item">View Link</a></div>;
+
+
 		return <div><input name={this.props.path} styles={styles} ref="focusTarget" value={this.props.value} onChange={this.valueChanged} autoComplete="off" className="form-control" /><a href={this.props.value} className="btn btn-link btn-goto-linked-item">View Link</a></div>;
 	}
 });
